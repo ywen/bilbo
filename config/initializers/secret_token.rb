e42733d2a9c8b770c51e880fba0a335a609e8ad8
Yi Wen <ywen@whiteglove.com>
2011-06-11 23:51:08 -0500

    added mongoid config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bilbo::Application.config.secret_token = '2eadfc2ec84f92ec5d22f7875754967850805012d7c4b53064269b4572e185d07b10282b21424f712eebf0c47e083c5875545ca509adc27bed1580f5c60ca641'
