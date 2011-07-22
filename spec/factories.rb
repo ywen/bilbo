4cc5849ed200726399731253e40dd728ff94af37
Yi Wen <hayafirst@gmail.com>
2011-07-21 22:17:12 -0500

    removed unused spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,8 @@
 FactoryGirl.define do
-  sequence :user_login do |n|
-    "login_#{n}"
-  end
-  sequence :user_name do |n|
-    "name_#{n}"
-  end
-  sequence :user_email do |n|
-    "user#{n}@example.com"
-  end
   factory :user do |u|
-    u.login{Factory.create :user_login}
-    u.name {Factory.create :user_name}
-    u.email {Factory.create :user_email}
+    sequence(:login) {|n| "login#{n}"}
+    sequence(:name) {|n| "Name#{n}"}
+    sequence(:email) {|n| "email#{n}@example.com"}
     u.password {"password"}
   end
 end
