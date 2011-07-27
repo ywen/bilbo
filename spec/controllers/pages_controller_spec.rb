a5054ab75f343f06bd76dd486d35bc28c47147b1
Yi Wen <hayafirst@gmail.com>
2011-07-27 16:26:17 -0500

    more specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,10 +5,9 @@ describe PagesController do
     let(:page1) {double :page1}
     let(:page2) {double :page2}
     context "when the user logged in" do
-      let(:user) {Factory.build :user}
+      let(:user) {Factory.create :user}
       before(:each) do
         Page.stub(:all).and_return [page1, page2]
-        @request.env["devise.mapping"] = Devise.mappings[:admin]
         sign_in :user, user
       end
 
@@ -20,6 +19,13 @@ describe PagesController do
       it "should assign pages" do
         get :index
         assigns[:pages].should == [page1, page2]
+      end
+    end
+
+    context "when the user hasn't logged in" do
+      it "should redirect to the sign_in page" do
+        get :index
+        response.should redirect_to("/users/sign_in")
       end
     end
 
