48836bfd649a116e052c71e9815febc68537443b
Yi Wen <hayafirst@gmail.com>
2011-07-28 00:06:21 -0500

    try to fix build

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :user do |u|
-    sequence(:login) {|n| "login#{n}"}
     sequence(:name) {|n| "Name#{n}"}
     sequence(:email) {|n| "email#{n}@example.com"}
     u.password {"password"}
